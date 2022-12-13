ec0d4173d45e634e8f7504deb440e6b2b10fc34e
margarita <ritatokareva99@mail.ru>
2022-12-13 16:27:43 +0500

    Упражнение - Расчет проекта

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
-console.log('Hello world!');
+const rate = 80;
+const hourseDay = 5;
+const daysWeek = 5;
+
+const order = 40;
+const holiday = 11;
+
+let priceWork = rate * order;
+console.log('Стоимость работы: ' + priceWork + '$');
+let doWork = (holiday - 2) * hourseDay >= order;
+console.log('Успею выполнить работу? ' + doWork);

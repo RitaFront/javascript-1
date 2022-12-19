d36d255c7b40c4216bf3f135749c31d7d094c134
margarita <ritatokareva99@mail.ru>
2022-12-19 19:07:56 +0500

    Кредит на macbook

--- app.js
+++ app.js
@@ -1,10 +1,38 @@
-// Переписать функцию в стрелочную
+// const priceMacbook = 2000;
+// const ageUser = 25;
+// const jobUser = true;
+// let moneyUser = 1500;
 
-function toPower(num, power) {
-  const res = num ** power;
-  return res;
+function credit(age, job) {
+  if (age > 24 && job) {
+    return 500;
+  } else if (age > 24) {
+    return 100;
+  } else {
+    return 0;
+  }
 }
-console.log(toPower(2, 2));
 
-const toPowerArrow = (num, power) => num ** power;
-console.log(toPowerArrow(2, 2));
+// function buyMacbook(age, job, moneyUser, priceMacbook) {
+//   if (moneyUser >= priceMacbook) {
+//     console.log('Пользователь может купить Macbook :)');
+//   } else {
+//     const allmoney = credit(age, job) + moneyUser;
+//     if (allmoney >= priceMacbook) {
+//       console.log('Пользователь может купить Macbook :)');
+//     } else {
+//       console.log('Пользователь не сможет купить Macbook :(');
+//     }
+//   }
+// }
+
+// buyMacbook(25, false, 1900, 2000);
+
+//более простое решение
+
+function buyMacbook(age, job, moneyUser, priceMacbook) {
+  const creditMoney = credit(age, job);
+  return priceMacbook <= moneyUser + creditMoney;
+}
+
+console.log(buyMacbook(25, false, 1900, 2000));

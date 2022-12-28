d5fe607a869ff222f98868eb90660e1328c72dbc
margarita <ritatokareva99@mail.ru>
2022-12-28 17:15:52 +0500

    Реализация some

--- app.js
+++ app.js
@@ -1,11 +1,28 @@
-const arr = [1, 4, 4, 10];
+const arr = [2, 4, 4, 10, 20];
 
-const avg = arr.reduce((acc, value, i) => {
-  if (i != arr.length - 1) {
-    return acc + value;
-  } else {
-    return (acc + value) / arr.length;
-  }
-}, 0);
+//мое решение
+// function some(arr, el) {
+//   let isOk = false;
+//   for (const elArr of arr) {
+//     if (el === elArr) {
+//       isOk = !isOk;
+//       break;
+//     }
+//   }
+//   return isOk;
+// }
 
-console.log(avg);
+// console.log(some(arr, 1));
+
+//более правильное и короткое решение
+
+// function some(array, element) {
+//   let res = array.find((el) => el === element);
+//   return res == undefined ? false : true;
+// }
+
+// console.log(some(arr, 0));
+
+//решение с помощью метода some
+
+console.log(arr.some((el) => el === 10));

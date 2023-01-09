946a91b6e40fc7e09d8bb2de6dbd400a2848aa25
margarita <ritatokareva99@mail.ru>
2023-01-09 18:11:38 +0500

    Кошелек

--- app.js
+++ app.js
@@ -1,23 +1,29 @@
-const users = [
-  {
-    name: 'Вася',
-    surname: 'Пупкин',
-    age: 30,
-    skills: ['Разработка', 'Готовка'],
+const wallet = {
+  balance: 0,
+  operations: [],
+  positiveBalance: function (reason, sum) {
+    this.balance += sum;
+    this.operations.push({ reason, sum });
+    return true;
   },
-  {
-    name: 'Катя',
-    surname: 'Белова',
-    age: 18,
-    skills: ['Дизайн'],
+  negativeBalance: function (reason, sum) {
+    if (this.balance < sum) {
+      return false;
+    }
+    this.balance -= sum;
+    this.operations.push({ reason, sum });
+    return true;
   },
-];
+  sumOperations: function () {
+    return this.operations.length;
+  },
+};
 
-let userData = users.map((user) => {
-  return {
-    fullName: `${user.name} ${user.surname}`,
-    skillsNum: user.skills.length,
-  };
-});
-
-console.log(userData);
+console.log(wallet.positiveBalance('Зп', 1000));
+console.log(wallet.sumOperations());
+console.log(wallet.negativeBalance('Долг', 2000));
+console.log(wallet.sumOperations());
+console.log(wallet.negativeBalance('Покупка', 500));
+console.log(wallet.sumOperations());
+console.log(wallet.operations);
+console.log(wallet.balance);

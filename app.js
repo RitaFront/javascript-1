a56e657aba459f7c56ce259ba43deb938b347408
margarita <ritatokareva99@mail.ru>
2023-01-09 15:33:26 +0500

    Преобразование объектов

--- app.js
+++ app.js
@@ -1,8 +1,23 @@
 const users = [
-  { name: 'Вася', age: 30 },
-  { name: 'Катя', age: 18 },
-  { name: 'Аня', age: 40 },
-  { name: 'Петя', age: 25 },
+  {
+    name: 'Вася',
+    surname: 'Пупкин',
+    age: 30,
+    skills: ['Разработка', 'Готовка'],
+  },
+  {
+    name: 'Катя',
+    surname: 'Белова',
+    age: 18,
+    skills: ['Дизайн'],
+  },
 ];
 
-console.log(users.sort((a, b) => a.age - b.age));
+let userData = users.map((user) => {
+  return {
+    fullName: `${user.name} ${user.surname}`,
+    skillsNum: user.skills.length,
+  };
+});
+
+console.log(userData);
